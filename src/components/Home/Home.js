d872e5ad4f0f46c46a9380bc3b08b2df7dbed075
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2021-04-25 15:14:29 +0200

    Home component and CSS added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import Product from '../Product/Product';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img 
+                    className="home__image"
+                    src="https://images-eu.ssl-images-amazon.com/images/G/03/kindle/journeys/YTNkYTJjMWMt/YTNkYTJjMWMt-YTMyZjZjMzAt-w1500._CB657354263_.jpg"
+                    alt="Image"
+                />
+
+                <div className="home__row">
+                    <Product 
+                        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"
+                        price={19.99}
+                        rating= {4}
+                        image="https://m.media-amazon.com/images/I/81vvgZqCskL._AC_UY327_FMwebp_QL65_.jpg"
+
+                    />
+                    <Product 
+                        title="Philips UI Coffee Machine"
+                        price="299.90"
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81xlewDHCoL._AC_SY879_.jpg"
+                    />
+                </div>
+                <div className="home__row">
+                    <Product 
+                        title="Tommy Hilfiger 1781976 Ladies’ Multi-Dial Quartz Watch with Stainless Steel Strap"
+                        price="139.72"
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/811ESn6Gk6L._AC_UL480_FMwebp_QL65_.jpg"
+                    />
+                    <Product 
+                        title="Lenovo IdeaPad 3 Chromebook Laptop 35.6 cm (14 Inches, 1366 x 768, HD, Anti-Glare) Slim Notebook (Intel Celeron N4020, 4GB RAM, 64GB eMMC, Intel UHD Graphics 600, ChromeOS) Silver"
+                        price="559.99"
+                        rating={3}
+                        image="https://m.media-amazon.com/images/I/413BhtTJ7nL._AC_UY327_FMwebp_QL65_.jpg"
+                    />
+                    <Product 
+                        title="Vans Women's Wm Ward trainers"
+                        price="52.72"
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/71qSCIK2uRL._AC_UL480_FMwebp_QL65_.jpg"
+                    />
+                </div>
+                <div className="home__row">
+                    <Product 
+                        title="Dyon LED TV (Triple Tuner (DVB-C/-S2/-T2), Hotel Mode, USB Media Player)"
+                        price="219.99"
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/81LZI4f4x5L._AC_UY327_FMwebp_QL65_.jpg"
+                    />
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Product/Product.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Product.css'
+
+
+function Product({title, price, rating, image}) {
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p className="product__title">{title}</p>
+                <p className="product__price">
+                    <small>€</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating).fill(
+                        <span class="fa fa-star checked"></span>
+                    )}
+                
+                </div>
+                <img 
+                    className="product__image"
+                    src={image}
+                    alt="Product Image"                
+                />
+            </div>
+            <button className="product__button">
+                Add to Basket
+            </button>
+        </div>
+    )
+}
+
+export default Product
