22ab7b770a8d64476dd80340640f0e8099705209
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2021-06-21 13:50:35 +0200

    fetch data from firestore db

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../context/firebase'
+import { useStateValue } from '../../context/StateProvider';
 import './Orders.css'
 
 
 function Orders() {
+    const [orders, setOrders] = useState([])
+    const [{user, basket}, dispatch] = useStateValue();
+
+    useEffect(() => {
+        if(user){
+            db
+            .collection('users')
+            .doc(user?.uid)
+            .collection('orders')
+            .orderBy('created', 'desc')
+            .onSnapshot(snapshot => (
+                setOrders(snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    data: doc.data()
+                })))
+            ))
+        }
+        else{
+            setOrders([])
+        }
+    }, [user])
+
     return (
         <div className="orders">
             <h1>Your Orders</h1>
